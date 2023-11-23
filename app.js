94d912e7b051f274a695fdb4f8a9d5a717e71f2c
Lay182 <jinwoo9288@gmail.com>
2023-11-23 10:55:03 +0900

    Project Done

---
+++ app.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('content-2').addEventListener('click', function () {
+        document.querySelector('.Counselor').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById('service-2').addEventListener('click', function () {
+        document.querySelector('.service-img').scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('content-3').addEventListener('click', function () {
+        document.querySelector('.Counselor').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById('service-3').addEventListener('click', function () {
+        document.querySelector('.service-img').scrollIntoView({ behavior: 'smooth' });
+    });
+});
